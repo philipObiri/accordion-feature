fe75efee9a736ca03c7ee15c1222c7a77fb1500b
Tesla Telson <philipobiri3.1@gmail.com>
2023-09-03 13:00:38 +0000

    Updated

--- src/App.js
+++ src/App.js
@@ -27,20 +27,20 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null)
   return (
-    <div clasName="accordion">
-      {data.map((el, i) => (<AccordionItem title={el.title} text={el.text} num={i} key={el.title}> </AccordionItem>))}
+    <div className="accordion">
+      {data.map((el, i) => (<AccordionItem curOpen={curOpen} onOpen={setCurOpen} title={el.title} num={i} key={el.title}>{el.text} </AccordionItem>))}
     </div>
   )
-
 }
 
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
 
   function handleToggle() {
-    setIsOpen(!isOpen);
+    onOpen(isOpen ? null : num);
   }
 
   return (
@@ -49,7 +49,10 @@ function AccordionItem({ num, title, text }) {
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? '-' : '+'}</p>
 
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   )
 }
+
+
+
